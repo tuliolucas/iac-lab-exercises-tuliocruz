5fe59b38210833e37c5363c6a675d19b43d7d54f
Tulio Cruz <tulio.cruz@ext.typeform.com>
2025-10-20 15:29:12 +0200

    SESSION_3: Create the s3 for the remot state in a different directory.

---
+++ backend_support/variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  type        = string
+  description = "Prefix for resource names"
+  default = "tuliocruz-iac-lab"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-north-1"
+}
+

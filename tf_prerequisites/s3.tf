6d261929a8855cc56c3647287afee3e040c8bdec
Tulio Cruz <Tulio.Cruz@easyjet.com>
2025-11-27 16:47:09 +0100

    Session 6: Create iam-github.tf successfully created, step 5.

---
+++ tf_prerequisites/s3.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "${var.prefix}-tfstate"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+  
+  tags = {
+    Name = format("%s-tfstate", var.prefix)
+  }
+}
+
+//Enable bucket versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "tfstate_bucket_versioning" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+//Ensure that all public access is blocked
+resource "aws_s3_bucket_public_access_block" "tfstate_bucket_public_access_block" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+//Ensure that the S3 bucket is using encryption algorithm AES256
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_bucket_encryption" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+ 
---
+++ tf_prerequisites/variables.tf
@@ -0,0 +1,18 @@
+variable "prefix" {
+  type        = string
+  description = "Prefix for resource names"
+  default = "tuliocruz-iac-lab"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-north-1"
+}
+
+variable "repo_name" {
+  type        = string
+  description = "GitHub repository name"
+  default = "tuliolucas/infra-as-code-assignment-tuliocruz"
+}
+
